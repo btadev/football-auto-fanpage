617b9a47266943722a32fed6a9b1a8cb113c57fd
Majeed Thaika <majeed.thaika@hotmail.com>
2017-02-19 15:24:23 +0700

    Added dynamic bg

--- assets/js/getbg.js
+++ assets/js/getbg.js
@@ -1,4 +1,3 @@
-
 $('document').ready(function() {
   var googleImgAPI = "https://www.googleapis.com/customsearch/v1?"+
                      "key=AIzaSyCLh1L38w368FuBjf5aWOI6uhrsTw0YR4o"+
@@ -6,15 +5,17 @@ $('document').ready(function() {
                      "&num=1"+
                      "&imgSize=huge"+
                      "&q=%22"+
-                     "liverpool+fc"+
+                    //  "liverpool+fc"+
+                     "rc+tours"+
                      "%22+wallpaper+background";
+  $.ajaxSetup({async : false});
   $.getJSON( googleImgAPI, function( data, status, xhr ) {
-    var imgURL = null
+    var imgURL = null;
     if (data.searchInformation.totalResults == "0"){
-      imgURL = "assets/images/uefa.jpg"
+      imgURL = "assets/images/uefa.jpg";
     } else {
       imgURL = data.items[0].pagemap.cse_image[0].src;
     }
-    console.log(imgURL);
+    $('body').css('background-image', "url("+imgURL+")");
   });
 });
