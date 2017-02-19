9558888d70f1d55334ddc2fd384a6f87fe2d88b7
Majeed Thaika <majeed.thaika@hotmail.com>
2017-02-19 16:31:55 +0700

    added extra google api key

--- assets/js/getbg.js
+++ assets/js/getbg.js
@@ -1,14 +1,22 @@
 function setBackground(team_name) {
   var googleImgAPI = "https://www.googleapis.com/customsearch/v1?"+
-                     "key=AIzaSyCLh1L38w368FuBjf5aWOI6uhrsTw0YR4o"+
-                     "&cx=014307518238632820624:qhfpncr2fzy"+
+                     "key=AIzaSyDaEpCda0c3l9puPg_jco5i2CEbMGX8dao"+
+                     "&cx=017366758790753236853:jj2goca3fss"+
                      "&num=1"+
                      "&imgSize=huge"+
                      "&q=%22"+
                      team_name.replace(" ", "+")+
-                     "%22+football+wallpaper+background";
+                     "%22+wallpaper+football+background";
+  // var googleImgAPI = "https://www.googleapis.com/customsearch/v1?"+
+  //                    "key=AIzaSyCLh1L38w368FuBjf5aWOI6uhrsTw0YR4o"+
+  //                    "&cx=014307518238632820624:qhfpncr2fzy"+
+  //                    "&num=1"+
+  //                    "&imgSize=huge"+
+  //                    "&q=%22"+
+  //                    team_name.replace(" ", "+")+
+  //                    "%22+wallpaper+football+background";
   console.log( $(team_name));
-  $.ajaxSetup({async : false});
+  $.ajaxSetup({async : true});
   $.getJSON( googleImgAPI, function( data, status, xhr ) {
     if (data.searchInformation.totalResults != "0"){
       var imgURL = data.items[0].pagemap.cse_image[0].src;
